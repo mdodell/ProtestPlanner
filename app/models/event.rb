cec18c3f8a9f50ea073301382e668a9e84190101
JadeNotJaded <jadeng66@gmail.com>
2020-02-26 13:38:47 -0500

    aded user controller

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,24 @@
 class Event < ApplicationRecord
     has_many :user_event_relationships
     has_many :users, through: :user_event_relationships
+
+    #backend method to find the closest 5 (or less if there are not 5 close events)
+    #events to user (in params)
+    def findNearbyEvents(current_user_id)
+        nearby = {} #this dict stores the event and its distance from users location
+        Event.all do | id, location_lat, location_long |
+            distance = Geocoder::Calculations.distance_between(
+                [User.find(current_user_id).location_lat, User.find(current_user_id).location_long],
+                [location_lat, location_long])
+            # if distance < 10
+            nearby[:id] = distance
+            # end
+        end
+        nearby.sort_by {|_key, value| value}
+        if nearby.length > 5
+            return a[0..5]
+        else 
+            return nearby
+        end
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,20 @@
 class User < ApplicationRecord
     has_many :user_event_relationships
     has_many :events, through: :user_event_relationships
+
+    #backend method to find the events that specified user is an organizer
+    #of
+    # def eventsUserOrganizes(current_user_id)
+    #     organizer_events = []
+    #     UserEventRelationship.all do | user_id, event_id, role_type_id |
+    #         if user_id == current_user_id and role_type_id == 1
+    #             organizer_events.push(event_id)
+    #         end
+    #     end
+    #     return organizer_events
+    # end
+
+
 end
+
+
