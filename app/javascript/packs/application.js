87db83350f78ba43d27cb431c548f927ce70ae36
dodellmitchell@gmail.com <YOUR@EMAIL.com>
2020-02-28 15:15:21 -0500

    Got bootstrap and font-awesome working

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+import "./custom";
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
@@ -15,3 +16,7 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+import "bootstrap";
+import "../../assets/stylesheets/custom"
+import "@fortawesome/fontawesome-free/js/all";
