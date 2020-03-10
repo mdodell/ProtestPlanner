c99b7aa23f55903544dd07521008c205b1bb8422
ttaver <taungon@brandeis.edu>
2020-03-10 16:46:58 -0400

    messing w routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,7 @@ Rails.application.routes.draw do
 
   get "signup", to: "users#new", as: "signup"
   get "login", to: "sessions#new", as: "login"
-  get "logout", to: "sessions#destroy", as: "logout"
+  get "logout", to: "sessions#destroy", as: "logout" 
+
+  #routes draft
 end
