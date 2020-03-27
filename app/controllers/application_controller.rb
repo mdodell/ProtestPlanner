341ac3486d6ad5b4453e3145ec4157798e9c3bbd
danielzhangcs <danielzhang@brandeis.edu>
2020-03-27 14:55:14 -0400

    forwards after login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,9 @@ class ApplicationController < ActionController::Base
 
   def require_login
     unless logged_in?
-      # flash[:error] = "You must be logged in to access this section"
-      redirect_to login_path# halts request cycle
+      store_location
+      flash[:danger] = "Please log in."
+      redirect_to login_path
     end
   end
 
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -42,4 +42,15 @@ module SessionsHelper
     cookies.delete(:remember_token)
   end
 
+  # Redirects to stored location (or to the default).
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # Stores the URL trying to be accessed.
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
+
 end
