c57834560e4642117b4a72fa947e1aaea5a42e60
JadeNotJaded <jadeng66@gmail.com>
2020-03-29 17:15:44 -0400

    added simple search in nav

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   resources :account_activations, only: [:edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get  '/events/new',  to: 'events#new'
-  get  '/browse',  to: 'events#index'
   post '/events/:id/signup', to: 'events#register', as: 'register'
   delete '/events/:id/unregister', to: 'events#unregister', as: 'unregister'
   get '/home', to: 'home#index'
@@ -15,7 +14,9 @@ Rails.application.routes.draw do
   get    '/profile',   to: 'users#profile'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
+  get '/search' => 'pages#search', :as => 'search_page'
   post '/setUserLocation', to: 'users#setUserLocation'
+  
 
   root 'home#index'
 
