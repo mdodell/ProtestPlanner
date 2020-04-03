01d55faa4ec0f1c869807a4b39dc38a1d2f8071f
dodellmitchell@gmail.com <YOUR@EMAIL.com>
2020-04-03 14:27:25 -0400

    Refactored routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,22 +2,24 @@ Rails.application.routes.draw do
   resources :users
   resources :events
   resources :account_activations, only: [:edit]
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get  '/events/new',  to: 'events#new'
+
+  get '/home', to: 'events#index', as: 'home'
+
   post '/events/:id/signup', to: 'events#register', as: 'register'
   delete '/events/:id/unregister', to: 'events#unregister', as: 'unregister'
   get 'events/:id/map', to: 'events#map', as: 'map'
-  get '/home', to: 'events#index', as: 'home'
-
-  get 'sessions/new'
-  get  '/signup',  to: 'users#new'
-  get    '/login',   to: 'sessions#new'
-  get    '/profile',   to: 'users#profile'
-  post   '/login',   to: 'sessions#create'
-  delete '/logout',  to: 'sessions#destroy'
-  get '/search' => 'pages#search', :as => 'search_page'
   get '/search_events', to: 'events#search'
   post '/search_events', to: 'events#search'
+
+  get  '/signup',  to: 'users#new'
+  get    '/profile',   to: 'users#profile'
+
+  get 'sessions/new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
+  get '/search' => 'pages#search', :as => 'search_page'
   post '/setUserLocation', to: 'users#setUserLocation'
   
 
