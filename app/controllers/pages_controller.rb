2d03a1c49033f7b99e5c3bc3b1ce372a7b588eea
JadeNotJaded <jadeng66@gmail.com>
2020-04-05 18:35:57 -0400

    fixed broken link on light nav bar and added a link to browse

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
 
     def search  
         if params[:search].blank?  
-            redirect_to(root_path, alert: "Empty field!") and return  
+            redirect_to(root_path, alert: "Type in a keyword to search for an event") and return  
         else 
             @parameter = params[:search].downcase  
             @results = Event.all.where("lower(name) like ?", "%#{@parameter}%") 
