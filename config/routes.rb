a6b2fa4caf92caa0eb5bc1d9c8e4b25b558a5de6
JadeNotJaded <jadeng66@gmail.com>
2020-04-07 10:37:33 -0400

    added photos

--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,7 @@ Rails.application.routes.draw do
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
 
+
   get '/search' => 'pages#search', :as => 'search_page'
   post '/setUserLocation', to: 'users#setUserLocation'
   
