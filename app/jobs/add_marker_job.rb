374bc802f39f25af816a504db4ee9b2e666ab9df
dodellmitchell@gmail.com <YOUR@EMAIL.com>
2020-04-16 12:52:49 -0400

    Updated add_marker_job

--- app/jobs/add_marker_job.rb
+++ app/jobs/add_marker_job.rb
@@ -2,8 +2,6 @@ class AddMarkerJob < ApplicationJob
   queue_as :default
 
   def perform(marker)
-    sleep 1
     ActionCable.server.broadcast("marker_channel", marker)
-    # Do something later
   end
 end
