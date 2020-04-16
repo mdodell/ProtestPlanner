948a04c010f2ccce3d6dd8216c46007cbc9c57d6
dodellmitchell@gmail.com <YOUR@EMAIL.com>
2020-04-16 13:17:37 -0400

    Set up redis

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,6 +93,10 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  # Configure action cable
+  config.web_socket_server_url = "wss://rise-up-brandeis.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://rise-up-brandeis.herokuapp.com', 'http://rise-up-brandeis.herokuapp.com']
+
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
   # middleware. The `delay` is used to determine how long to wait after a write
