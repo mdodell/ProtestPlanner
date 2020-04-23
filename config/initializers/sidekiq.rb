0e123b480b167debb251b9b0c46ad16ae9ab5d3f
danielzhangcs <danielzhang@brandeis.edu>
2020-04-23 17:00:08 -0400

    modify config

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -27,9 +27,9 @@ require 'sidekiq/web'
 # end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV['REDIS_URL'] || 'redis://localhost:6379/1' }
+  config.redis = { url: ENV['REDISTOGO_URL'] || 'redis://localhost:6379/1' }
 end
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV['REDIS_URL'] || 'redis://localhost:6379/1' }
+  config.redis = { url: ENV['REDISTOGO_URL'] || 'redis://localhost:6379/1' }
 end
