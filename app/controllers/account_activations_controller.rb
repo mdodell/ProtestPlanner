b52ddadbd558b1d19198f63bce0dc0360df804b8
dodellmitchell@gmail.com <YOUR@EMAIL.com>
2020-05-09 18:34:33 -0400

    Got rid of console.logs and flash

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -2,17 +2,14 @@ class AccountActivationsController < ApplicationController
   skip_before_action :require_login, only: [:edit]
   def edit
     user = User.find_by(user_name: params[:user_name])
-    puts "haha"
     puts user.id
     if user && !user.activated? && user.authenticated?(:activation, params[:id])
-      puts "here I am"
       user.activate
       log_in user
       flash[:success] = "Account activated!"
       redirect_to root_url
     else
-      puts "cry"
-      flash[:danger] = "Invalid activation link"
+      #flash[:danger] = "Invalid activation link"
       redirect_to login_url
     end
   end
